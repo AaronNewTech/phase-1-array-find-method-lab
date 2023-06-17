0655dec955e8a9323108684defe4c135c1e9ccb7
Aaron <arnhsmith@gmail.com>
2023-06-17 09:50:59 -0400

    finished and tested code

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+
+
+function superbowlWin(record) {
+    
+    
+    const w = record.find(({ result }) => result === 'W');
+    console.log(w)
+    if (w === undefined) {
+        return w
+    }
+    else {
+        return w.year
+    }
+    
+}
